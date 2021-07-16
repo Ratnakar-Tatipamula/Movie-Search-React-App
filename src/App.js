2f21cfa3f376a7800de517dff518a94aeb72cb60
Ratnakar <tatipamularatnakar98@gmail.com>
2021-07-16 12:02:13 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
     <div className="App">
-      <h3>Movie Search App</h3>
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Movie Search App</li>
+        <li>Create Account</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
